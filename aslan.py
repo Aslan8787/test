7dbb2f1f087ae62307b05e09e5c5fd30f13e2aba
Aslan8787 <118472051+Aslan8787@users.noreply.github.com>
2022-11-18 00:50:38 +0300

    test
    test

---
+++ aslan.py
@@ -0,0 +1,16 @@
+name = input("Как тебя зовут? ")
+print(f"Привет, {name}")
+surname = input("Как твоя фамилия? ")
+age = int(input("Сколько тебе лет? "))
+if age >=18:
+    print(f"Добро пожаловать на наш сайт, {surname}, {name}")
+else:
+    print(f"{surname}, {name}, Вы не можете заходить на данный сайт!")
+
+
+
+
+
+
+
+
